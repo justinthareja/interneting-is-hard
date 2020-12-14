ec7dbc5b6b359c96cfcd8bcb7f9e96b6a7eb4448
Justin Thareja <justinthareja@gmail.com>
2020-12-14 11:20:26 -0500

    Added toyProblems folder

---
+++ toyProblems/vowelCount.js
@@ -0,0 +1,31 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// My Solution
+function vowelCount(string = []) {
+    let counter = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const letters = string.split('');
+
+    letters.forEach(letter => {
+        vowels.forEach(vowel => {
+            if (letter === vowel) {
+                counter++;
+            }
+        });
+    });
+
+    return counter;
+}
+
+// Top Solution:
+// function getCount(str) {
+//     return (str.match(/[aeiou]/ig)||[]).length;
+// }
+  
+
+console.log(vowelCount('heyo maggots'), 'should equal 4');
+console.log(vowelCount('looooooong word including spaces'), 'should equal 13');
